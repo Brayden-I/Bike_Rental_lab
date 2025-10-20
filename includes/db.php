22e6cafa7879f5b32b63031734840f90231149aa
BraydenI <bi1012440@otc.edu>
2025-10-20 10:17:38 -0500

    started intial files and functions

---
+++ includes/db.php
@@ -0,0 +1,15 @@
+<?php
+    function db() {
+        try {
+            $dsn = "mysql:host=localhost;bs_bikeshop=introtodb;charset=utf8mb4";
+            $user = "root";
+            $pass = "";
+            
+            return new PDO($dsn, $user, $pass, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ]);
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+            exit();
+        }
+    }
---
+++ includes/functions.php
@@ -0,0 +1,46 @@
+<?php
+// functions.php
+function sqlAllCustomers(): string
+{
+    return "SELECT customer_id, first_name, last_name FROM customers ORDER BY last_name, first_name";
+}
+
+function sqlBikeRentals(): string
+{
+    return "SELECT * FROM rentals 
+            JOIN customers ON rentals.customer_id = customers.customer_id 
+            JOIN bikes ON rentals.bike_id = bikes.bike_id";
+}
+
+function sqlMorningRentals(): string
+{
+    return "SELECT * FROM rentals 
+            JOIN customers ON rentals.customer_id = customers.customer_id 
+            JOIN bikes ON rentals.bike_id = bikes.bike_id
+            WHERE rentals.rental_time < '12:00:00'";
+}
+
+function sqlTop3Bikes(): string
+{
+    return "SELECT bikes.bike_id, bikes.model, COUNT(rentals.rental_id) AS rental_count
+            FROM bikes
+            JOIN rentals ON bikes.bike_id = rentals.bike_id
+            GROUP BY bikes.bike_id, bikes.model
+            ORDER BY rental_count DESC
+            LIMIT 3";
+}
+
+function sqlOpenRentals(): string
+{
+    return "SELECT * FROM rentals 
+            JOIN customers ON rentals.customer_id = customers.customer_id 
+            JOIN bikes ON rentals.bike_id = bikes.bike_id
+            WHERE rentals.end_time IS NULL";
+}
+function sqlAllJoins(): string
+{
+    return "SELECT * FROM rentals 
+            JOIN customers ON rentals.customer_id = customers.customer_id 
+            JOIN bikes ON rentals.bike_id = bikes.bike_id
+            JOIN bike_types ON bikes.type_id = bike_types.type_id";
+}
